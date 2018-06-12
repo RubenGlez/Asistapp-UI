48222c69eb44c023fc58ecbf55eedebe63bf933f
Rubén González Alonso <rubenglez@MacBook-Pro-de-Ruben.local>
2018-06-12 20:52:51 +0200

    Creando el perfil y los formularios de login y registro

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+
+
+jQuery(document).ready(function(){ 
+
+	$('.carousel').flickity({
+		// options
+		cellAlign: 'left',
+		contain: true,
+		prevNextButtons: false,
+		pageDots: false
+	});
+
+});
